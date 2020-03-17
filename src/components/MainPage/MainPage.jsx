365792a02877a2361bf3a578e06044d1e74b74c1
Dmytro Serdiukov <dmytro.serdiukov@gmail.com>
2020-03-17 18:22:29 +0200

    started development of main page

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import mainpage from './mainpage.module.css';
+
+const MainPage = props => {
+    return (
+        <div className = {mainpage.window}>
+            
+        </div>
+    );
+}
+
+export default MainPage;
